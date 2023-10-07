93c5f032416241278ceee97d02188e33b65bf68c
aceveron <aceveron@gmail.com>
2023-10-07 14:12:42 +0800

    added about page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,30 @@ import { BsCart3, BsMoonFill, BsSunFill } from 'react-icons/bs';
 import { FaBarsStaggered } from 'react-icons/fa6';
 import { NavLink } from 'react-router-dom';
 import NavLinks from './NavLinks';
+import { useEffect, useState } from 'react';
+
+const themes = {
+  winter: 'winter',
+  dracula: 'dracula',
+};
+
+const getThemeFromLocalStorage = () => {
+  return localStorage.getItem('theme') || themes.winter;
+};
 
 const Navbar = () => {
+  const [theme, setTheme] = useState(getThemeFromLocalStorage());
+  const handleTheme = () => {
+    const { winter, dracula } = themes;
+    const newTheme = theme === winter ? dracula : winter;
+
+    setTheme(newTheme);
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
   return (
     <nav className='bg-base-200'>
       <div className='navbar align-element'>
@@ -35,6 +57,13 @@ const Navbar = () => {
         </div>
         <div className='navbar-end'>
           {/* THEME SETUP */}
+          <label className='swap swap-rotate'>
+            <input type='checkbox' onChange={handleTheme} />
+            {/* sun */}
+            <BsSunFill className='swap-on h4 w-4' />
+            {/* moon */}
+            <BsMoonFill className='swap-off h4 w-4' />
+          </label>
           {/* CART LINK */}
           <NavLink to='/cart' className='btn btn-ghost btn-circle btn-md ml-4'>
             <div className='indicator'>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,28 @@
 const About = () => {
-  return <h1 className='text-4xl'>About</h1>;
+  return (
+    <>
+      <div className='flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center'>
+        <h1 className='text-4xl font-bold leading-none tracking-tight sm:text-6xl'>
+          We Love
+        </h1>
+        <div className='stats bg-primary shadow'>
+          <div className='stat'>
+            <div className='stat-title text-primary-content text-4xl font-bold tracking-widest'>
+              comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className='mt-6 text-lg leading-8 max-w-2xl mx-auto'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        quaerat distinctio soluta quos obcaecati suscipit, natus vero ex commodi
+        eum quasi, provident optio accusantium magni qui consectetur dicta porro
+        cupiditate praesentium voluptates neque, ipsam amet molestias? Unde
+        nihil laudantium eveniet ducimus rem perferendis officiis possimus
+        consequatur saepe pariatur ipsa deleniti voluptates amet quas, iure
+        minus cupiditate ullam maxime, accusantium dicta?
+      </p>
+    </>
+  );
 };
 export default About;
